a8531d185be294649b9081b369faf2a8dbff0702
flockoloco <art913@hotmail.com>
2020-10-25 15:10:20 +0000

    Movemnt animation blablabla
    Just the shake pls

--- The Twins/Assets/Script/PlayerMovement.cs
+++ The Twins/Assets/Script/PlayerMovement.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Camera cam;
+
+
+    Vector2 movement;
+    Vector2 mousePos;
+    
+
+    void FixedUpdate()
     {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 }
