9302adcaab48377c875325f6d3bfec7a5bdf2b2c
Clickba <66256524+Clickba@users.noreply.github.com>
2020-12-02 12:25:09 +0000

    this one is shit

--- The Twins/Assets/Script/cameramovement.cs
+++ The Twins/Assets/Script/cameramovement.cs
@@ -23,9 +23,11 @@ public class cameramovement : MonoBehaviour
         {
             mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
             playerPos = player.transform.position;
+
             mouseDir = new Vector2(mousePos.x - playerPos.x, mousePos.y - playerPos.y).normalized;
             mouseDist = Mathf.Sqrt((mousePos.x - playerPos.x) * (mousePos.x - playerPos.x) + (mousePos.y - playerPos.y) * (mousePos.y - playerPos.y));
             finalvector = mouseDir * mouseDist / 8;
+
             gameObject.transform.position = new Vector3(finalvector.x + playerPos.x, finalvector.y + playerPos.y, -10);
         }
     }
