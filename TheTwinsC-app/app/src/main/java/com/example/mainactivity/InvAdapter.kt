b7efecd10171fe2007d02a10c83638c495dd28c3
flockoloco <art913@hotmail.com>
2020-12-11 09:00:38 +0000

    hue

--- TheTwinsC-app/app/src/main/java/com/example/mainactivity/InvAdapter.kt
+++ TheTwinsC-app/app/src/main/java/com/example/mainactivity/InvAdapter.kt
@@ -6,9 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class InvAdapter (var itemInv: List<Inventory>) : RecyclerView.Adapter<InvAdapter.InvViewHolder>() {
+class InvAdapter (var itemInv: List<Inventory>, private val clickListener: (Inventory) -> Unit): RecyclerView.Adapter<InvAdapter.InvViewHolder>() {
 
     inner class InvViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
@@ -22,11 +23,10 @@ class InvAdapter (var itemInv: List<Inventory>) : RecyclerView.Adapter<InvAdapte
     }
 
     override fun onBindViewHolder(holder: InvViewHolder, position: Int) {
-        holder.itemView.apply{
+        holder.itemView.apply {
             findViewById<TextView>(R.id.invName).text = itemInv[position].name
             findViewById<TextView>(R.id.invDescription).text = itemInv[position].details
-            findViewById<ImageView>(R.id.invIcon).imageAlpha = itemInv[position].icon
+            findViewById<ImageView>(R.id.invIcon).setImageResource(itemInv[position].icon)
         }
     }
-
 }
--- TheTwinsC-app/app/src/main/java/com/example/mainactivity/InventoryFragment.kt
+++ TheTwinsC-app/app/src/main/java/com/example/mainactivity/InventoryFragment.kt
@@ -3,8 +3,10 @@ package com.example.mainactivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,10 +28,14 @@ class InventoryFragment : Fragment(R.layout.fragment_inventory) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var invList = mutableListOf(
-                Inventory("blablabla", "blablabla", 2)
+                Inventory("blablabla", "blablabla", R.drawable.ic_ingot)
         )
         val invRecycler = view?.findViewById<RecyclerView>(R.id.invRecycler)
-        invRecycler.adapter = InvAdapter(invList)
+        invRecycler.adapter = InvAdapter(invList) { inventory -> personItemClicked(inventory) }
         invRecycler.layoutManager = LinearLayoutManager(this.activity)
+    }
+
+    private fun personItemClicked(inventory: Inventory){
+        Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
     }
 }
